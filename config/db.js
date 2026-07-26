83f4b9b9ba6b9eee75dac59eb07b221b3c601816
Shaveen08 <shaveenkumar0804@gmail.com>
2026-07-26 16:02:59 +0530

    Server and Databse Configured

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
 // Database Config
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully..");
+  } catch (err) {
+    console.log("Database failed to connect..", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,27 @@
+// Server
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// ------------------------------------------------------------
+// Middleware
+//  -----------------------------------------------------------
+app.use(cors());
+app.use(express.json());
+
+// ------------------------------------------------------------
+// Database connection
+//  -----------------------------------------------------------
+connectDB();
+
+// ------------------------------------------------------------
+// Server creation
+//  -----------------------------------------------------------
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: http://localhost:${PORT}`);
+});
